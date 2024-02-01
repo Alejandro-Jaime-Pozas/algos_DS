6a079c0544ebf70a3256633c69c429de3a81ee77
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-02-01 10:56:09 -0600

    format

--- arrays.py
+++ arrays.py
@@ -10,6 +10,6 @@ for n in new_list:
 
 # arr.insert() is O(n) since each value needs to be moved an index
 # arr.append() is O(1) since its added to the end of array
-# arr.delete() is O(n) since needs to move all the indexes over
+# arr.remove() is O(n) since needs to move all the indexes over
 
 # append is O(1) bc of how python interprets it

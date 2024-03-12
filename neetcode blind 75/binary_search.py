d044e5b96814a803e2d8cec8b40d54f6b28dcc42
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-03-12 12:15:41 -0600

    linked lists

--- neetcode blind 75/binary_search.py
+++ neetcode blind 75/binary_search.py
@@ -1,21 +1,25 @@
-def search(nums, target):
-    # check if target num is in rotated list in O(log n) time
-    # could choose a midpoint. based on that midpoint, compare midpoint and l/r
-    # if target between l and m, check left side, else check right side
-    res = -1
-    l, r = 0, len(nums)-1
-    while l <= r:
-        m = (l + r) // 2
-        if target == nums[m]: 
-            res = m 
-            break
-        elif target > nums[m] and (target <= nums[r] or target >= nums[r] <= nums[m]):
-            l = m + 1
-        else:
-            r = m - 1
-    return res 
+# def search(nums, target):
+#     # check if target num is in rotated list in O(log n) time
+#     # could choose a midpoint. based on that midpoint, compare midpoint and l/r
+#     # if target between l and m, check left side, else check right side
+#     res = -1
+#     l, r = 0, len(nums)-1
+#     while l <= r:
+#         m = (l + r) // 2
+#         for n in (m,l,r):
+#             if target == nums[n]:
+#                 res = n
+#                 break
+#         # here trying to say "if l < target < m OR l > m > target OR target > l > m"
+#         if nums[l] < target < nums[m] \
+#         or nums[l] > nums[m] > target \
+#         or target > nums[l] > nums[m]:
+#             r = m - 1
+#         else:
+#             l = m + 1
+#     return res 
 
-print(search([7,8,4,4,5,6], 6))
+# print(search([7,2,4,4,5,6], 5))
 
 
 # def findMin(nums):
